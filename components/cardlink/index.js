869ef5cd65b8ca6b19aaa5856dfc9947bf615d79
rycont <reactdev@kakao.com>
2020-10-05 15:41:46 +0900

    feat : add cardlink component

---
+++ components/cardlink/index.js
@@ -0,0 +1,55 @@
+import { LitElement, html, css } from "https://unpkg.com/lit-element?module";
+
+class CardLink extends LitElement {
+  static get properties() {
+    return {
+      title: {
+        type: String,
+      },
+      text: {
+        type: String,
+      },
+      iconSrc: {
+        type: String,
+      },
+    };
+  }
+  static get styles() {
+    return css`
+      div.wrapper {
+        border: 2px solid #dbdbdb;
+        padding: 20px 24px;
+      }
+      div.header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+      }
+      img {
+        fill: "818181";
+      }
+      h1 {
+        font-size: 18px;
+        margin: 0px;
+      }
+      p {
+        margin: 12px 0px 0px 0px;
+        font-size: 18px;
+      }
+    `;
+  }
+  render() {
+    console.log(this, this.text);
+    return html`
+      <div class="wrapper">
+        <div class="header">
+          <h1>${this.title}</h1>
+          <img src="${this.iconSrc}" />
+        </div>
+        <p>${this.text}</p>
+      </div>
+    `;
+  }
+}
+
+export default CardLink;
