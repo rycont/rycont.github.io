1f384ab81140bb9060e9e7b9f9100029c4f10963
rycont <reactdev@kakao.com>
2020-10-05 22:37:33 +0900

    docs : add projects

--- components/projects/index.js
+++ components/projects/index.js
@@ -15,6 +15,8 @@ class Projects extends LitElement {
       }
       img {
         display: block;
+        min-height: 41px;
+        width: 100%;
       }
       div.tag {
         font-family: "Noto Sans KR", sans-serif;
@@ -57,6 +59,15 @@ class Projects extends LitElement {
       .도커 {
         background-color: #5f99e6;
       }
+      .노드 {
+        background-color: #62a761;
+      }
+      .크롬 {
+        background-color: #cc6463;
+      }
+      .NPM {
+        background-color: #ce2f2c;
+      }
       h1 {
         font-weight: 900;
         font-size: 22px;
@@ -86,7 +97,7 @@ class Projects extends LitElement {
         color: white;
         border-radius: 6px;
         box-shadow: 0px 0px 18px rgba(0, 0, 0, 0.18);
-        opacity: 0.3;
+        opacity: 0.5;
         transition: 1s var(--fast-animate);
         margin: 6px;
       }
@@ -96,7 +107,7 @@ class Projects extends LitElement {
         transform: translateY(-10px);
       }
       .wrapper.selected > a > div {
-        opacity: 0.3;
+        opacity: 0.1;
       }
       a > div.card_wrapper.selected {
         opacity: 1;
@@ -106,16 +117,64 @@ class Projects extends LitElement {
   projects = [
     {
       name: "엄랭",
-      tags: ["언어", "TS", "디노"],
+      tags: ["언어", "TS", "디노", "노드"],
       descript: "인명으로 만들어진 프로그래밍 언어",
       image: "/assets/image/um.svg",
       link: "https://github.com/rycont/umjunsik-lang",
     },
     {
       name: "베네판다",
-      tags: ["RN", "GCP", "도커"],
+      tags: ["RN", "GCP", "도커", "TS"],
       descript: "베네듀 안드로이드 클라이언트 (비공식)",
       image: "/assets/image/benepanda.svg",
+      link: "https://github.com/DIMI19WP/benepanda",
+    },
+    {
+      name: "디미고인",
+      tags: ["리액트", "TS"],
+      descript: "디미고 교내 인트라넷 프론트엔드 개발",
+      image: "/assets/image/dimigoin.svg",
+      link: "https://github.com/dimigoin/dimigoin-front-v3/",
+    },
+    {
+      name: "조사컴플릿",
+      tags: ["노드", "TS", "NPM"],
+      descript: "한국어 조사를 완성해주는 자바스크립트 라이브러리",
+      link: "https://github.com/rycont/josa-complete",
+    },
+    {
+      name: "SelfCheckV2",
+      tags: ["노드", "TS"],
+      descript: "리뉴얼된 자가진단 자동화",
+      link: "https://github.com/rycont/nodejs-selfcheck-automation-v2",
+      image: "/assets/image/selfcheck.png",
+    },
+    {
+      name: "FlowChat",
+      tags: ["노드", "리액트", "TS"],
+      descript: "카카오톡 웹 클라이언트(개발중)",
+      link: "https://github.com/flowchat-dev",
+      image: "/assets/image/flowchat.png",
+    },
+    {
+      name: "Gnex",
+      tags: ["노드", "언어", "NPM"],
+      descript: "준비된 한국형 언어",
+      link: "https://github.com/rycont/Gnex",
+    },
+    {
+      name: "AlwaysJS",
+      tags: ["언어"],
+      image: "/assets/image/alwaysjs.png",
+      descript: "웹 자바스크립트 IDE",
+      link: "https://github.com/rycont/Gnex",
+    },
+    {
+      name: "BingShin",
+      tags: ["크롬"],
+      image: "/assets/image/bingshin.png",
+      descript: "구글 => 빙 리디렉션 익스텐션",
+      link: "https://github.com/rycont/Gnex",
     },
   ];
   static get properties() {
@@ -163,9 +222,9 @@ class Projects extends LitElement {
                 <img src="${e.image}" />
                 <div class="content_wrapper"></div>
                 <div class="tags">
-                  ${e.tags.map(
-                    (tag) => html` <div class="tag ${tag}">${tag}</div> `
-                  )}
+                  ${e.tags
+                    .slice(0, 2)
+                    .map((tag) => html` <div class="tag ${tag}">${tag}</div> `)}
                 </div>
                 <h1>${e.name}</h1>
                 <p>${e.descript}</p>
